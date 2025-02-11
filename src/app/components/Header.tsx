28466bfba0cf2169044d8864cf12acbcb471414a
Ahmed-Abbasi-Official <farhad.khi17@gmail.com>
2025-02-11 16:03:48 +0500

    Navbar

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,11 +1,33 @@
 import styles from '@/app/styles/navbar.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
+import Nav from './Nav'
 
 const Header = () => {
   return (
-    <header className={styles.main_header}>
+    <header className={`${styles.main_header}`}>
+
+        {/*=========
+            LOGO  
+         ===========*/}
+
         <div className={styles.navbar_brand}>
-            <h1>Ahmed</h1>
+            <Link href="/">
+            <Image 
+            src='/logo.png'
+            alt='Logo'
+            width={150}
+            height={40}
+            />
+            </Link>
         </div>
+
+        {/*=========
+           NAV MENU 
+         ===========*/}
+
+          <Nav/>
+
     </header>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,14 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "./components/Header";
+import { Mulish } from 'next/font/google'
+ 
+// If loading a variable font, you don't need to specify the font weight
+const mulish = Mulish({
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,10 +31,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en"className={mulish.className}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Header/>
         {children}
       </body>
     </html>
