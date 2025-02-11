2cd9d7687733f92915f8d52a532a8b6a33dd9d8c
Ahmed-Abbasi-Official <farhad.khi17@gmail.com>
2025-02-11 15:06:18 +0500

    setup project

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,13 @@
+import styles from '@/app/styles/navbar.module.css'
+
+const Header = () => {
+  return (
+    <header className={styles.main_header}>
+        <div className={styles.navbar_brand}>
+            <h1>Ahmed</h1>
+        </div>
+    </header>
+  )
+}
+
+export default Header
